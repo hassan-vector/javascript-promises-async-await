10bd6f5aeb2cf9d8b8722b896c01c7f93e3f60cb
Hassan Sadiq <Hassan.Sadiq@vector.co.nz>
2022-10-10 23:45:44 +1300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
- export function fetchMovies()
+import {fetchWithTimeout} from "./services";
+
+const movies = require('./data/movies.json');
+
+export function fetchMovies()
  {
+  const resolveFunction = () => movies;
+
+  fetchWithTimeout(1000).then(resolveFunction);
  }
+
+var moviePromise = Promise(function(fetchMovies){
+ fetchMovies();
+});
+
+moviePromise.then(
+    function(results){console.log(results);}
+)
