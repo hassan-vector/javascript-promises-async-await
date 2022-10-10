e7da17c57754e3b798c2c2b4b2664ec9a364cffa
Hassan Sadiq <Hassan.Sadiq@vector.co.nz>
2022-10-10 23:50:54 +1300

    corected promise

--- src/index.js
+++ src/index.js
@@ -5,14 +5,10 @@ const movies = require('./data/movies.json');
 export function fetchMovies()
  {
   const resolveFunction = () => movies;
-
-  fetchWithTimeout(1000).then(resolveFunction);
+  return fetchWithTimeout(1000).then(resolveFunction);
  }
 
-var moviePromise = Promise(function(fetchMovies){
- fetchMovies();
+const moviePromise =  fetchMovies();
+moviePromise.then(results => {
+ console.log(results);
 });
-
-moviePromise.then(
-    function(results){console.log(results);}
-)
