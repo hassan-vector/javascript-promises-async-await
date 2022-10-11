2f8704043b4755880abdac1f93e090169ee04d88
Hassan Sadiq <Hassan.Sadiq@vector.co.nz>
2022-10-11 22:35:41 +1300

    module 5

--- src/services.js
+++ src/services.js
@@ -10,6 +10,16 @@ export function fetchMovies(){
         .catch(error => console.log(error));
 }
 
+export async function asyncFetchMovies() {
+    try{
+        const response = await fetch("/data/movies.json");
+        const results = response.json();
+        return results;
+    }catch (error){
+        console.log(error);
+    }
+}
+
 export function fetchBooks(){
     return fetch("./data/books.json")
         .then(response => response.json())
