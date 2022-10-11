db905510a22d23ba9713dd10a618d4501ac29831
Hassan Sadiq <Hassan.Sadiq@vector.co.nz>
2022-10-11 16:16:39 +1300

    updated

--- src/index.js
+++ src/index.js
@@ -2,26 +2,25 @@ import {fetchWithTimeout, fetchMovies, fetchBooks} from "./services";
 
 const movies = require('./data/movies.json');
 
-function getBooksAndMovies(){
+const getBooksAndMovies = () => {
     return Promise.all([fetchBooks(), fetchMovies()])
         .then(([books, movies]) => ({
             books,
             movies
         }))
         .catch(error => console.log("Error fetching books and movies", error));
-}
+};
+
+const getBooksAndMoviesPromise = getBooksAndMovies();
+getBooksAndMoviesPromise.then(results => {
+    console.log('getBooksAndMoviesPromise', results);
+});
 
 function getBooksOrMovies(){
     return Promise.race([fetchBooks(), fetchMovies()])
         .then(results => results)
         .catch(error => console.log("Error waiting for the promise race", error));
 }
-
-const getBooksAndMoviesPromise = getBooksAndMovies();
-
-getBooksAndMoviesPromise.then( results => {
-    console.log('getBooksAndMoviesPromise', results);
-});
 
 const getBooksOrMoviesPromise = getBooksOrMovies();
 getBooksOrMoviesPromise.then( results => {
--- src/services.js
+++ src/services.js
@@ -7,12 +7,12 @@ export function fetchMovies(){
     return fetch("./data/movies.json")
         .then(response => response.json())
         .then(movies => movies)
-        .catch(error => console(error));
+        .catch(error => console.log(error));
 }
 
 export function fetchBooks(){
     return fetch("./data/books.json")
         .then(response => response.json())
         .then(books => books)
-        .catch(error => console(error));
+        .catch(error => console.log(error));
 }
